1dfd18411d48fde114b1f38e1572b28444108505
Juliakuznetsovaaaa <99537178+Juliakuznetsovaaaa@users.noreply.github.com>
2024-07-14 12:37:53 +0300

    rebranding

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
-import "../reset.css";
-import "../styles.css";
-import ReactDOM from "react-dom";
-import React from "react";
-import Main from "./components/Main";
-import Header from "./components/Header";
+import {createRoot} from 'react-dom/client'
+import Main from './components/Main.jsx'
+import { StrictMode } from 'react'
+import Header from './components/Header.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(
-  <>
+
+createRoot(document.getElementById('header')).render(
+  <StrictMode>
     <Header />
+  </StrictMode>,
+)
+
+createRoot(document.getElementById('main')).render(
+  <StrictMode>
     <Main />
-  </>
-);
+  </StrictMode>,
+)
